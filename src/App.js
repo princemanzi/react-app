0fc51454e56d8c8225616e2721edda9c30f458d1
Prince Manzi <manziprincemp@gmail.com>
2025-02-18 20:36:19 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,41 @@
+import { useState } from 'react';
 import './App.css';
 
-const  App = () => {
+// const  App = () => {
+//   const name = 'John';
+//   const isNameShowing = true;
+//   return (
+//     <div className="App">
+//       <h1>Hello, {isNameShowing ? name : 'someone'}!</h1>
+//     </div>
+//   );
+// }
+// const Person = (props) => {
+//   return(
+//     <>
+//     <h1> Name: {props.name}</h1>
+//     <h2> Last Name: {props.lastName}</h2>
+//     <h2> Age: {props.Age}</h2>
+//     </>
+//   );
+// }
+
+// const App = () => {
+//   return (
+//     <div className = "App">
+//       <Person name = {'John'} lastName = {'Doe'} Age = {'30'}/>
+//       <Person name = "Mary" lastName = 'Doe' Age = {'24'}/>
+//     </div>
+//   );
+// }
+
+const App = () => {
+  const [counter, setCounter] = useState(0);
   return (
-    <div className="App">
-      <h1>Hello, React</h1>
+    <div className = "App">
+      <button onClick={() => setCounter((prevCount) => prevCount - 1)}>-</button>
+      <h1>{counter}</h1>
+      <button onClick={() => setCounter((prevCount) => prevCount + 1)}>+</button>
     </div>
   );
 }
