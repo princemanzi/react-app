471ecace275518213c4e78e811e8f45c4c82dc63
Prince Manzi <manziprincemp@gmail.com>
2025-02-19 19:09:11 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 
 // const  App = () => {
@@ -31,6 +31,10 @@ import './App.css';
 
 const App = () => {
   const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    setCounter(100);
+  }, []);
   return (
     <div className = "App">
       <button onClick={() => setCounter((prevCount) => prevCount - 1)}>-</button>
