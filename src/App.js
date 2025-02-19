c827348d1f6a788f13f0440efab11b2435723b80
Prince Manzi <manziprincemp@gmail.com>
2025-02-19 19:23:04 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,47 +1,9 @@
-import { useState, useEffect } from 'react';
-import './App.css';
-
-// const  App = () => {
-//   const name = 'John';
-//   const isNameShowing = true;
-//   return (
-//     <div className="App">
-//       <h1>Hello, {isNameShowing ? name : 'someone'}!</h1>
-//     </div>
-//   );
-// }
-// const Person = (props) => {
-//   return(
-//     <>
-//     <h1> Name: {props.name}</h1>
-//     <h2> Last Name: {props.lastName}</h2>
-//     <h2> Age: {props.Age}</h2>
-//     </>
-//   );
-// }
-
-// const App = () => {
-//   return (
-//     <div className = "App">
-//       <Person name = {'John'} lastName = {'Doe'} Age = {'30'}/>
-//       <Person name = "Mary" lastName = 'Doe' Age = {'24'}/>
-//     </div>
-//   );
-// }
+import React from "react";
 
 const App = () => {
-  const [counter, setCounter] = useState(0);
-
-  useEffect(() => {
-    setCounter(100);
-  }, []);
-  return (
-    <div className = "App">
-      <button onClick={() => setCounter((prevCount) => prevCount - 1)}>-</button>
-      <h1>{counter}</h1>
-      <button onClick={() => setCounter((prevCount) => prevCount + 1)}>+</button>
-    </div>
-  );
+    return (
+        <h1>App</h1>
+    );
 }
 
 export default App;
