59ab8eb1680e5773b8565cd43cc88e34465ea1b2
Prince Manzi <manziprincemp@gmail.com>
2025-02-19 20:57:16 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,23 @@
-import React from "react";
+import { useEffect } from "react";
+
+
+//8ac8ab8e
+
+const API_URL = 'http://www.omdbapi.com/?apikey=8ac8ab8e&';
 
 const App = () => {
+
+    const searchMovies =async (title) => { //async means asynchronous data which means it takes time to fetch these movies.
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+
+        console.log(data);
+    }
+
+
+    useEffect(() => {
+        searchMovies('Avengers');
+    }, []);
   return <h1>App</h1>;
 };
 
